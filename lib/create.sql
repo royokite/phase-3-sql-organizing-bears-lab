4f5c309acd8229245463d154f027531ae74af744
RoyOkite <royokite@gmail.com>
2022-10-30 15:44:06 +0300

    Completed assignment

---
+++ lib/create.sql
@@ -0,0 +1,12 @@
+-- Active: 1667129437495@@127.0.0.1@3306
+
+CREATE TABLE
+    bears (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        age INTEGER,
+        sex TEXT,
+        color TEXT,
+        temperament TEXT,
+        alive BOOLEAN
+    );
---
+++ lib/insert.sql
@@ -0,0 +1,137 @@
+-- Active: 1667129437495@@127.0.0.1@3306
+
+INSERT INTO
+    bears (
+        name,
+        age,
+        sex,
+        color,
+        temperament,
+        alive
+    )
+VALUES (
+        'Mr. Chocolate',
+        22,
+        'M',
+        'black',
+        'peaceful',
+        1
+    );
+
+INSERT INTO
+    bears (
+        name,
+        age,
+        sex,
+        color,
+        temperament,
+        alive
+    )
+VALUES (
+        'Rowdy',
+        17,
+        'M',
+        'brown',
+        'irritable',
+        1
+    );
+
+INSERT INTO
+    bears (
+        name,
+        age,
+        sex,
+        color,
+        temperament,
+        alive
+    )
+VALUES (
+        'Tabitha',
+        20,
+        'F',
+        'white',
+        'calm',
+        0
+    );
+
+INSERT INTO
+    bears (
+        name,
+        age,
+        sex,
+        color,
+        temperament,
+        alive
+    )
+VALUES (
+        'Sergeant Brown',
+        20,
+        'M',
+        'black',
+        'quiet',
+        0
+    );
+
+INSERT INTO
+    bears (
+        name,
+        age,
+        sex,
+        color,
+        temperament,
+        alive
+    )
+VALUES (
+        'Melissa',
+        14,
+        'F',
+        'brown',
+        'white',
+        1
+    );
+
+INSERT INTO
+    bears (
+        name,
+        age,
+        sex,
+        color,
+        temperament,
+        alive
+    )
+VALUES (
+        'Grinch',
+        5,
+        'M',
+        'white',
+        'gloomy',
+        0
+    );
+
+INSERT INTO
+    bears (
+        name,
+        age,
+        sex,
+        color,
+        temperament,
+        alive
+    )
+VALUES (
+        'Wendy',
+        15,
+        'F',
+        'black',
+        'tempered',
+        1
+    );
+
+INSERT INTO
+    bears (
+        age,
+        sex,
+        color,
+        temperament,
+        alive
+    )
+VALUES (11, 'M', 'brown', 'fiesty', 0);
